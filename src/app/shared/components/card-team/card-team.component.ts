3940a09d2ed32dbcda698c25400e028a9e876856
Jhorman Orozco <jsojhor@gmail.com>
2023-08-31 13:38:48 -0500

    some changes

--- src/app/shared/components/card-team/card-team.component.ts
+++ src/app/shared/components/card-team/card-team.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Content } from 'src/app/models';
+import { PrivateService } from './../../../pages/private/service/private.service';
 
 @Component({
   selector: 'app-card-team',
@@ -8,4 +10,14 @@ import { Content } from 'src/app/models';
 })
 export class CardTeamComponent {
   @Input() team!: Content;
+
+  constructor(private router: Router, private privateService: PrivateService){}
+
+  redirectToEdit(id: number) {
+    this.router.navigate(['edit-team/', id])
+  }
+
+  deleteTeam(id: number){
+    this.privateService.deleteTeamById(id);
+  }
 }
